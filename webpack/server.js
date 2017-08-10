cbd386244f9294dd686a9a0e9d70f81528fd4ea7
Nancy <422196189@qq.coom>
2017-08-10 13:07:17 +0800

    webpack

---
+++ webpack/server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config.js');
+const constants = require('./constants');
+
+
+new WebpackDevServer(webpack(config), {
+  contentBase: constants.ABSOLUTE_BASE,
+  hot: true,
+  historyApiFallback: true,
+  proxy: {
+    '/api/*': {
+       target: 'http://192.168.7.164:8000/', //rap
+      pathRewrite: {
+        '^/api': `/mockjsdata/${constants.PROJECT_ID}/api`
+      },
+      secure: false,
+      changeOrigin: true,
+    }
+  },
+  stats: {colors: true}
+}).listen(constants.PORT, constants.HOST, function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(`Listening at ${constants.HOST}:${constants.PORT}/`);
+});
